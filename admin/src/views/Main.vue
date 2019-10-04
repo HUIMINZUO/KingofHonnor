d95243c6851fd525488eaf78c8b34c7b39861dc0
HUIMINZUO <zjb15779770973@163.com>
2019-10-04 19:39:01 +0800

    2019-10-04-19:38已完成图片框的操作

--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -9,7 +9,6 @@
             <el-menu-item index="/categories/create">新建分类</el-menu-item>
             <el-menu-item index="/categories/list">分类列表</el-menu-item>
           </el-menu-item-group>
-          
           <el-menu-item-group>
             <template slot="title">物品</template>
             <el-menu-item index="/items/create">新建物品</el-menu-item>
--- serve/index.js
+++ serve/index.js
@@ -5,6 +5,8 @@ const app = express()
 // 跨域模块
 app.use(require('cors')())
 app.use(express.json())
+// 静态托管文件
+app.use('/uploads',express.static(__dirname + '/uploads'))
 
 require('./plugins/db')(app)
 require('./routes/admin')(app)
